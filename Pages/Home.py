5ff07f0279743eabb9d9a4d992d86702f88c73e7
John Raymond <john.raymond.us@mail.ru>
2024-12-01 18:07:08 +0500

    Overhauled the entire Home page (Home.py), so that we have a description of our project.

--- Pages/Home.py
+++ Pages/Home.py
@@ -1,7 +1,40 @@
 import streamlit as st
 
+
 class Home:
     def __init__(self):
         pass
+
     def app(self):
-        st.write('Home')
+        st.title('Introduction to the project')
+        st.write("---")
+        st.write("""
+
+                 This project is aimed at expanding our understanding in dataframe analysis.
+                 For this module, our group of three have created different visualizations through the use of
+                 libraries that have been explored during ISTA 131 course, such as:
+                 - Pandas
+                 - Matplotlib
+                 - Numpy
+                 As well as auxiliary required libraries:
+                 -Streamlit
+                 """
+
+                 )
+        st.write("---")
+        st.subheader("Used environment")
+        st.write("""
+                 In order to offer the easier perception of our visualizations, we've
+                 decided to use the streamlit framework, which allowed us to organize
+                 our work through the use of several pages, one for every member of
+                 our team. 
+
+                 """)
+        st.write("---")
+        st.subheader("Repository information")
+        st.write("""
+                        If you wish to review the structure of the content as well as
+                        check the sequence of our git actions, you can visit our github
+                        repository at: 
+                        """)
+        st.write("[Github link >](https://github.com/ligushka789/ista131_Fp)")
